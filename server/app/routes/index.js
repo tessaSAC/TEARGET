c402dfa5b139c590c9b64fee2d7b39cdadbe12d0
Rachel Addleman <addlemanrachel@gmail.com>
2016-09-12 18:16:57 -0400

    Defined routes for men and tears

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,6 +3,8 @@ var router = require('express').Router(); // eslint-disable-line new-cap
 module.exports = router;
 
 router.use('/members', require('./members'));
+router.use('/tears', require('./tears'));
+router.use('/men', require('./men'));
 
 // Make sure this is after all of
 // the registered routes!
