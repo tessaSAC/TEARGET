ab55702932f71b73f364e65af83b4913d344883e
Rachel Addleman <addlemanrachel@gmail.com>
2016-09-15 12:22:57 -0400

    Added products state, ctrl and factory

---
+++ browser/js/products/products.js
@@ -0,0 +1,46 @@
+app.config(function ($stateProvider) {
+    $stateProvider.state('products', {
+        url: '/',
+        templateUrl: 'js/products/products.html', 
+        controller: 'ProductsCtrl'
+    });
+});
+
+app.controller('ProductsCtl', function($scope, $state, ProductFactory){
+    ProductFactory.getAll()
+    .then(function(products){
+        $scope.products = products;
+    })
+    $scope.error = null;
+})
+
+app.factory('ProductFactory', function($http){
+    var getAll = function(){
+        $http.get('/api/tears')
+        .then( function(response){
+            return response.data;
+        });        
+    var getOneTear = function(tearId){
+        $http.get('/api/tears/' + tearId)
+        .then( function(response){
+            return response.data
+        });
+    var getManTears = function(manId){
+        $http.get('/api/man/' + manId + '/tears')
+        .then( function(response){
+            return response.data
+        });
+    var getStateTears = function(state){
+        $http.get('/api/tears/' + state)
+        .then( function(response){
+            return response.data
+        });
+    var getOrganicTears = function(organic){
+        $http.get('/api/tears' + organic)
+        .then( function(response){
+            return response.data
+        });
+    }
+    }
+    }
+});
