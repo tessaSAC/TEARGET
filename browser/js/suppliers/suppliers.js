a8b6d36b07e76d1f7e93c7967b1698e8d5b4dff8
Rachel Addleman <addlemanrachel@gmail.com>
2016-09-19 09:27:43 -0400

    Supplier states functioning & linked

--- browser/js/suppliers/suppliers.js
+++ browser/js/suppliers/suppliers.js
@@ -3,17 +3,33 @@ app.config(function ($stateProvider) {
     $stateProvider.state('suppliers', {
         url: '/suppliers',
         templateUrl: 'js/suppliers/suppliers.html',
-        controller: 'SuppliersCtrl'
+        controller: 'SuppliersCtrl',
+        resolve: {
+            suppliers: function(SupplierFactory){
+                return SupplierFactory.getAll();
+            }
+        }
+    });
+    $stateProvider.state('supplier', {
+        url: '/suppliers/:id',
+        templateUrl: 'js/suppliers/supplier.html',
+        controller: 'SupplierCtrl',
+        resolve: {
+            supplier : function(SupplierFactory, $stateParams){
+                return SupplierFactory.getOneMan($stateParams.id);
+            }
+        }
     });
 });
 
-app.controller('SuppliersCtrl', function($scope, $state, SupplierFactory){
-    SupplierFactory.getAll()
-    .then(function(suppliers){
-        $scope.suppliers = suppliers;
-    })
-    $scope.error = null;
+
+app.controller('SuppliersCtrl', function($scope, suppliers){
+    $scope.suppliers = suppliers;
 });
+
+app.controller('SupplierCtrl', function($scope, supplier){
+    $scope.supplier = supplier;
+})
 
 app.factory('SupplierFactory', function($http){
     var getAll = function(){
