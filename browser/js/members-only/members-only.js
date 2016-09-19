7e7074e3690e2a397c4003155068b31dd17e0faa
Rachel Addleman <addlemanrachel@gmail.com>
2016-09-19 18:08:09 -0400

    Beginning attempt at an admin state.

--- browser/js/members-only/members-only.js
+++ browser/js/members-only/members-only.js
@@ -1,32 +1,32 @@
 app.config(function ($stateProvider) {
 
-    $stateProvider.state('membersOnly', {
-        url: '/members-area',
-        template: '<img ng-repeat="item in stash" width="300" ng-src="{{ item }}" />',
-        controller: function ($scope, SecretStash) {
-            SecretStash.getStash().then(function (stash) {
-                $scope.stash = stash;
+    $stateProvider.state('admin', {
+        url: '/admin',
+        templateUrl: 'js/members-only/admin.html',
+        controller: function ($scope, UserFactory, ProductFactory) {
+            UserFactory.getUsers()
+            .then(function (users){
+                $scope.users = users;
             });
-        },
-        // The following data.authenticate is read by an event listener
-        // that controls access to this state. Refer to app.js.
-        data: {
-            authenticate: true
+            ProductFactory.getAll()
+            .then( function(products){
+                $scope.products = products;
+            })
         }
-    });
 
+    });
 });
 
-app.factory('SecretStash', function ($http) {
+app.factory('UserFactory', function ($http) {
 
-    var getStash = function () {
-        return $http.get('/api/members/secret-stash').then(function (response) {
+    var getUsers = function () {
+        return $http.get('/api/user').then(function (response) {
             return response.data;
         });
     };
 
     return {
-        getStash: getStash
+        getUsers
     };
 
 });
