8edf0c3ad6b19c7b3130eabe6b1fab2c15be0583
tessaSAC <tessaSAC@users.noreply.github.com>
2016-09-20 12:50:26 -0400

    instantiate HTTPS server

--- server/main.js
+++ server/main.js
@@ -1,29 +1,35 @@
 'use strict';
-var chalk = require('chalk');
-var db = require('./db');
 
-// Create a node server instance! cOoL!
-var server = require('http').createServer();
+// commands to create key.pem, cert.pem:
+// openssl genrsa -out key.pem 2048
+// openssl req -x509 -new -nodes -key key.pem -days 1024 -out cert.pem
 
-var createApplication = function () {
-    var app = require('./app')(db);
-    server.on('request', app); // Attach the Express application.
-    require('./io')(server);   // Attach socket.io.
-};
+const chalk = require('chalk'),
+	  db = require('./db');
 
-var startServer = function () {
+// HTTPS Server Stuff:
+const https = require('https'),
+	  fs = require('fs'),
+	  PORT = process.env.PORT || 1337,
+	  secureConfig = {
+	  	key: fs.readFileSync(__dirname + '/../key.pem'),
+	  	cert: fs.readFileSync(__dirname + '/../cert.pem')
+	  },
+	  app = require('./app')(db);
 
-    var PORT = process.env.PORT || 1337;
 
-    server.listen(PORT, function () {
-        console.log(chalk.blue('Server started on port', chalk.magenta(PORT)));
-    });
-
-};
-
-db.sync()
-.then(createApplication)
-.then(startServer)
-.catch(function (err) {
-    console.error(chalk.red(err.stack));
+const server = https.createServer(secureConfig).listen(PORT, function(err) {
+	console.log(chalk.green('HTTPS server started on port'), chalk.magenta(PORT));
+	if (err) throw err;
+	db.sync()
+	.then(createApplication)
+	.catch(function(err) {
+		console.error(chalk.red(err.stack));
+	});
 });
+
+const createApplication = function() {
+	server.on('request', app)  // Attach the Express application
+	require('./io')(server);  // Attach socket.io
+	console.log('Made app!');
+}
