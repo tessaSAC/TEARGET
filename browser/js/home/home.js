6b33a14e7ba4fc0551de3dae16c26ad4670ba9e3
tessaSAC <tessaSAC@users.noreply.github.com>
2016-09-21 21:51:06 -0400

    add carousel

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,6 +1,10 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
-        templateUrl: 'js/home/home.html'
-    });
+        templateUrl: 'js/home/home.html',
+        controller: function ($scope, FullstackPics) {
+		    // Images of beautiful Fullstack people.
+		    $scope.images = _.shuffle(FullstackPics);
+	    }
+	});
 });
